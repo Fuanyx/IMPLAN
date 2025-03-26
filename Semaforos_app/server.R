c2bcea05d59aee08244e36febf168801b71c6fd7
Fuanyx <juanantonioyamlino@gmail.com>
2025-03-26 12:21:55 -0500

    Segunda versión de la aplicación para monitoreo de trafico en tiempo real.
    Esta versión ya tiene conexión directa a las bases de posgrestSQL y datos para las graficas de tiempos en las avenidas principales de la ciudad  de Cancún

--- Semaforos_app/server.R
+++ Semaforos_app/server.R
@@ -3,68 +3,42 @@ library(leaflet)
 library(httr)
 library(jsonlite)
 library(dplyr)
+library(RPostgres)
 
 # API Key de HERE Maps (Reemplázala si es necesario)
 API_KEY <- "zMvsNweQ4jp7FKQIV4WCeIeaT0rWtbM1GPzofanYGLk"
 
-# Lista de rutas con identificador, origen y destino
-#rutas_inicial <- data.frame(
-#  id = 1:24,
-#  origen = c("21.166578,-86.855914", "21.160493,-86.860974", "21.164213,-86.853738", "21.162237,-86.851633", 
-#             "21.164010,-86.853810", "21.159546,-86.856096", "21.164543,-86.846202", "21.159930,-86.849424", 
-#             "21.161586,-86.851451", "21.167457,-86.833764", "21.158810,-86.848344", "21.159769,-86.849423", 
-#             "21.156457,-86.856116", "21.153547,-86.842967", "21.158649,-86.848385", "21.152603,-86.841975", 
-#             "21.154680,-86.839498", "21.153109,-86.842730", "21.150730,-86.847411", "21.150932,-86.840287", 
-#             "21.152241,-86.841823", "21.149525,-86.838766", "21.143057,-86.846546", "21.153467,-86.838689"),
-#  destino = c("21.164187,-86.853997", "21.163996,-86.853891", "21.167180,-86.847722", "21.164060,-86.853681", 
-#              "21.161980,-86.851847", "21.161641,-86.851695", "21.162150,-86.851315", "21.161792,-86.851227", 
-#              "21.159878,-86.849540", "21.159894,-86.849274", "21.159821,-86.849323", "21.158764,-86.848521", 
-#             "21.158681,-86.848519", "21.158711,-86.848243", "21.153404,-86.843026", "21.153273,-86.842643", 
-#              "21.153361,-86.842593", "21.152554,-86.842146", "21.152432,-86.842161", "21.152347,-86.841729", 
-#              "21.150846,-86.840380", "21.150670,-86.840026", "21.150647,-86.840234", "21.150929,-86.840131")
-#)
+# Conexión a la base de datos
+con <- dbConnect(
+  Postgres(),
+  dbname = "siginplan",
+  host = "45.132.241.118",     
+  port = 5432,         
+  user = "juanyam",
+  password = "eJnNPmklNznIkZ1EJ8JB4B=="
+)
 
-rutas_inicial <- data.frame(
-  id = c(4, 2, 8, 9, 10, 11, 14, 17, 22, 23, 12, 20, 21, 19, 24, 1, 18, 5, 6, 7, 3, 13, 15, 16),
-  Principal = c("Av. Kabah", "Francisco I madero", "Av. Kabah", "Av. Kabah", "Chichen", "Av. Kabah",
-                  "Av. Kabah", "Coba", "Av. Kabah", "La luna", "Av. Kabah", "Av. Kabah", "Av. Kabah", 
-                  "Xcaret", "La luna", "Av. Kabah", "Av. Kabah", "Av. Kabah", "Portillo", "Portillo",
-                  "Francisco I madero", "Chichen", "Av. Kabah", "Av. Kabah"),
-  De = c("Av. José López Portillo", "Calle 103", "Av. Chichen-Itza", "Av. José López Portillo", "Uxmal",
-               "Av. Chichen-Itza 2", "Av. Cobá", "La costa", "Av. Mayapan", "Kohunlich", "Av. Chichen-Itza",
-               "La costa", "Av. Xcaret", "Del sol", "Xcaret", "Av. Miguel Hidalgo", "Av. Cobá",
-               "Av. Francisco I. Madero", "XUENCAL", "Calle 71", "Calle 71", "XUENCAL", "Av. Chichen-Itza 2",
-               "Av. Xcaret"),
-  A = c("Av. Francisco I. Madero", "AV kabah", "Av. José López Portillo", "Av. Chichen-Itza", "AV kabah",
-              "Av. Chichen-Itza", "Av. Chichen-Itza", "AV kabah", "La costa", "AV kabah", "Av. Chichen-Itza 2",
-              "Av. Xcaret", "La costa", "AV kabah", "La costa", "Av. Francisco I. Madero", "Av. Xcaret",
-              "Av. José López Portillo", "AV kabah", "AV kabah", "AV kabah", "AV kabah", "Av. Cobá", "Av. Cobá"),
-  origen = c("21.162237,-86.851633", "21.160493,-86.860974", "21.159930,-86.849424", "21.161586,-86.851451",
-             "21.167457,-86.833764", "21.158810,-86.848344", "21.153547,-86.842967", "21.154680,-86.839498",
-             "21.149525,-86.838766", "21.143057,-86.846546", "21.159769,-86.849423", "21.150932,-86.840287",
-             "21.152241,-86.841823", "21.150730,-86.847411", "21.153467,-86.838689", "21.166578,-86.855914",
-             "21.153109,-86.842730", "21.164010,-86.853810", "21.159546,-86.856096", "21.164543,-86.846202",
-             "21.164213,-86.853738", "21.156457,-86.856116", "21.158649,-86.848385", "21.152603,-86.841975"),
-  destino = c("21.164060,-86.853681", "21.163996,-86.853891", "21.161792,-86.851227", "21.159878,-86.849540",
-              "21.159894,-86.849274", "21.159821,-86.849323", "21.158711,-86.848243", "21.153361,-86.842593",
-              "21.150670,-86.840026", "21.150647,-86.840234", "21.158764,-86.848521", "21.152347,-86.841729",
-              "21.150846,-86.840380", "21.152432,-86.842161", "21.150929,-86.840131", "21.164187,-86.853997",
-              "21.152554,-86.842146", "21.161980,-86.851847", "21.161641,-86.851695", "21.162150,-86.851315",
-              "21.167180,-86.847722", "21.158681,-86.848519", "21.153404,-86.843026", "21.153273,-86.842643"),
-  Tipo = c("Vuelta", NA, "Vuelta", "Ida", NA, "Vuelta", "Vuelta", NA, "Vuelta", NA, "Ida", "Vuelta", "Ida", NA, NA, 
-           "Ida", "Ida", "Ida", NA, NA, NA, "Ida", "Ida", "Vuelta"),
-  "Tiempo" = c(0, 0, 0, 0,
-                 0, 0, 0, 0,
-                 0, 0, 0, 0,
-                 0, 0, 0, 0,
-                 0, 0, 0, 0,
-                 0, 0, 0, 0),
-  distancia = c("300 m", "850 m", "280 m", "280 m", "1800 m", "150 m",
-                 "800 m", "350 m", "180 m","1100 m", "150 m", "220 m",
-                 "220 m", "600 m", "350 m", "350 m", "86 m", "300 m",
-                 "500 m", "600 m", "700 m", "850 m", "800 m", "100 m") )
+semaforos <- dbGetQuery(con, 'SELECT * FROM "99_Evaluaciones"."Coordenadas_Semaforos_Sistema"')
+semaforos <- semaforos[,c(2,4,5)]
+colnames(semaforos) <- c("nombre","lat","lng")
 
-rutas_inicial <- subset(rutas_inicial, rutas_inicial$Principal == "Av. Kabah")
+
+trayectos <- dbGetQuery(con, 'SELECT * FROM "99_Evaluaciones"."Tiempo_traslado_semaforos"')
+trayectos$key <- paste(trayectos$Principal, trayectos$De)
+trayectos$Hora <- as.numeric(trayectos$Hora)
+#trayectos$Hora <- as.character(trayectos$Hora)
+trayectos$Principal <- gsub(" ", "",trayectos$Principal)
+trayectos$De <- gsub(" ", "",trayectos$De)
+trayectos <- trayectos[,c(1,2,7,3,8,9)]
+ida <- subset(trayectos, trayectos$Tipo == "Ida")
+vuelta <- subset(trayectos, trayectos$Tipo == "Regreso")
+ida$Tipo <- NULL
+vuelta$Tipo <- NULL
+
+
+coordenadas <- dbGetQuery(con, 'SELECT * FROM "99_Evaluaciones"."Trayectos_traslado_semaforos"')
+coordenadas$distancia <- paste(coordenadas$distancia, "m")
+coordenadas <- coordenadas[,c(1:7,9,8)]
 
 # Función para obtener el ETA desde HERE Maps
 obtener_eta <- function(origen, destino) {
@@ -86,49 +60,173 @@ obtener_eta <- function(origen, destino) {
 
 server <- function(input, output, session) {
   
-  rutas_reactivas <- reactiveVal(rutas_inicial)
+  semaforos_reactivos <- reactiveVal(semaforos)
+  coordenadas_reactivas <- reactiveVal(coordenadas)
+  
+  ida_reactivas <- reactiveVal(ida)
+  vuelta_reactivas <- reactiveVal(vuelta)
+  
+  
+  coordenadas_reactivas_ida <- reactiveVal(coordenadas_reactivas)
+  coordenadas_reactivas_vuelta <- reactiveVal(coordenadas_reactivas)
+  
+  grafico_actual <- reactiveVal("barras")
+  
   
   observeEvent(input$generar, {
-    nuevas_rutas <- rutas_inicial %>% mutate("Tiempo" = mapply(obtener_eta, origen, destino))
-    rutas_reactivas(nuevas_rutas)
+    nuevas_rutas <- coordenadas_reactivas() %>% 
+      mutate("Tiempo" = mapply(obtener_eta, origen, destino))  
+    
+    coordenadas_reactivas(nuevas_rutas)  
+  })
+
+  observeEvent(input$mostrar_Kabah, {
+    semaforos_filtrados <- filter(semaforos, nombre == "1. Av Kabah")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av.Kabah")
+    ida_filtradas <- filter(ida, Principal == "Av.Kabah")
+    vuelta_filtradas <- filter(vuelta, Principal == "Av.Kabah")
+    
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+    ida_reactivas(ida_filtradas)
+    vuelta_reactivas(vuelta_filtradas)
   })
   
+  observeEvent(input$mostrar_Xcaret, {
+    semaforos_filtrados <- filter(semaforos, nombre == "6. Av. Xcaret")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Xcaret")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  
+  observeEvent(input$mostrar_Quintana, {
+    semaforos_filtrados <- filter(semaforos, nombre == "5. Av. Andrés Quintana Roo")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Andres Quintana Roo")
+    ida_filtradas <- filter(ida, Principal == "Av. Andres Quintana Roo")
+    vuelta_filtradas <- filter(vuelta, Principal == "Av. Andres Quintana Roo")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+    ida_reactivas(ida_filtradas)
+    vuelta_reactivas(vuelta_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Kukulcan, {
+    semaforos_filtrados <- filter(semaforos, nombre == "2. Bld. Kukulcan")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "2. Bld. Kukulcan")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  observeEvent(input$mostrar_Portillo, {
+    semaforos_filtrados <- filter(semaforos, nombre == "3. Av. Lopez Portillo")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. López Portillo")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Chac, {
+    semaforos_filtrados <- filter(semaforos, nombre == "10. Av. Chac Mool")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Chac Mool")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Bonampak, {
+    semaforos_filtrados <- filter(semaforos, nombre == "8. Av. Bonampak")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Bonampak")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Nichupte, {
+    semaforos_filtrados <- filter(semaforos, nombre == "4. Av. Nichupté")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Nichupté")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Tulum, {
+    semaforos_filtrados <- filter(semaforos, nombre == "9. Av. Tulum")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Tulum")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  
+  observeEvent(input$mostrar_Coba, {
+    semaforos_filtrados <- filter(semaforos, nombre == "7. Av. Cobá")
+    coordenadas_filtradas <- filter(coordenadas, Sobre == "Av. Cobá")
+    semaforos_reactivos(semaforos_filtrados)
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Ida, {
+    coordenadas_filtradas <- filter(coordenadas_reactivas(), Trayecto == "Ida")
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+  
+  observeEvent(input$mostrar_Regreso, {
+    coordenadas_filtradas <- filter(coordenadas_reactivas(), Trayecto == "Regreso")
+    coordenadas_reactivas(coordenadas_filtradas)
+  })
+
+  # Botón para mostrar todos
+  observeEvent(input$mostrar_Todos, {
+    semaforos_reactivos(semaforos)
+    coordenadas_reactivas(coordenadas)
+  })
+  
+  # Renderizar el mapa
   output$mapa <- renderLeaflet({
     leaflet() %>%
-      addTiles() %>%
-      addCircleMarkers(data = rutas_inicial, 
-                       lat = as.numeric(sapply(strsplit(rutas_inicial$origen, ","), `[[`, 1)), 
-                       lng = as.numeric(sapply(strsplit(rutas_inicial$origen, ","), `[[`, 2)), 
-                       color = "blue", label = ~paste("Origen", id)) %>%
-      addCircleMarkers(data = rutas_inicial, 
-                       lat = as.numeric(sapply(strsplit(rutas_inicial$destino, ","), `[[`, 1)), 
-                       lng = as.numeric(sapply(strsplit(rutas_inicial$destino, ","), `[[`, 2)), 
-                       color = "red", label = ~paste("Destino", id))
+      addTiles()
   })
   
   observe({
-    nuevas_rutas <- rutas_reactivas()
-    
     leafletProxy("mapa") %>%
       clearMarkers() %>%
-      addCircleMarkers(data = nuevas_rutas, 
-                       lat = as.numeric(sapply(strsplit(nuevas_rutas$origen, ","), `[[`, 1)), 
-                       lng = as.numeric(sapply(strsplit(nuevas_rutas$origen, ","), `[[`, 2)), 
-                       color = "blue", label = ~paste("Origen", id)) %>%
-      addCircleMarkers(data = nuevas_rutas, 
-                       lat = as.numeric(sapply(strsplit(nuevas_rutas$destino, ","), `[[`, 1)), 
-                       lng = as.numeric(sapply(strsplit(nuevas_rutas$destino, ","), `[[`, 2)), 
-                       color = "red", label = ~paste("Destino", id))
-  })
+      setView(lng = mean(semaforos_reactivos()$lng), 
+              lat = mean(semaforos_reactivos()$lat), 
+              zoom = 13) %>%  
+      addCircleMarkers(
+        data = semaforos_reactivos(),
+        lat = ~lat, lng = ~lng,
+        color = "blue",
+        label = ~nombre
+      )
+  }) 
   
   output$tabla_resultados <- renderTable({
-    rutas_reactivas()[, c("De","A","distancia","Tipo","Tiempo"), drop = FALSE]
+    coordenadas_reactivas()[, c("De","a","distancia","Trayecto","Tiempo"), drop = FALSE]
   })
+  colnames(coordenadas)
   
   output$descargar <- downloadHandler(
     filename = function() { "ETA_Resultados.csv" },
     content = function(file) {
-      write.csv(rutas_reactivas(), file, row.names = FALSE)
+      write.csv(coordenadas_reactivas(), file, row.names = FALSE)
     })
+  
+  observeEvent(input$btn1, { grafico_actual("barras") })
+  
+  output$grafico1 <- renderPlot({
+    if (grafico_actual() == "barras") {
+      ggplot(ida_reactivas(), aes(x = Hora, y = `Tiempo promedio`, fill = reorder(De, ID))) +
+        geom_bar(stat = "identity") +
+        labs(title = "Tiempo en trayecto Ida",
+             x = "Hora", y = "Tiempo (minutos)", fill = "Av principal") +
+        theme_minimal()
+    } 
+  })
+  
+  output$grafico2 <- renderPlot({
+    if (grafico_actual() == "barras") {
+      ggplot(vuelta_reactivas(), aes(x = Hora, y = `Tiempo promedio`, fill = reorder(De, ID))) +
+        geom_bar(stat = "identity") +
+        labs(title = "Tiempo en trayecto Regreso",
+             x = "Hora", y = "Tiempo (minutos)", fill = "Av principal") +
+        theme_minimal()
+    } 
+  })
+  
 }
-
--- Semaforos_app/ui.R
+++ Semaforos_app/ui.R
@@ -2,16 +2,50 @@ library(shiny)
 library(leaflet)
 
 ui <- fluidPage(
-  titlePanel("Visualización de Rutas y Tiempos de Tráfico"),
+  titlePanel("Mapa y Cálculo de Distancias"),
   
   fluidRow(
-    column(5,  # Ocupa 4 de 12 columnas disponibles
+    column(8, offset = 4,  # Colocamos los botones alineados a la derecha
            actionButton("generar", "Calcular ETA"),
-           downloadButton("descargar", "Descargar Resultados"),
+           downloadButton("descargar", "Descargar Resultados")
+    )
+  ),
+  
+  # Primera fila de botones (toda la parte superior)
+  fluidRow(
+    column(12, 
+           actionButton("mostrar_Kukulcan", "Av. Kukulcan"),
+           actionButton("mostrar_Portillo", "Av. Lopez Portillo"),
+           actionButton("mostrar_Kabah", "Av. Kabah"),
+           actionButton("mostrar_Chac", "Av. Chac Mool"),
+           actionButton("mostrar_Bonampak", "Av. Bonampak"),
+           actionButton("mostrar_Nichupte", "Av. Nichupté"),
+           actionButton("mostrar_Tulum", "Av. Tulum"),
+           actionButton("mostrar_Quintana", "Av. Andrés Quintana Roo"),
+           actionButton("mostrar_Xcaret", "Av. Xcaret"),
+           actionButton("mostrar_Coba", "Av. Cobá"),
+           actionButton("mostrar_Todos", "Mostrar Todos")
+    )
+  ),
+  
+  # Segunda fila de botones (trayectos de ida y regreso)
+  fluidRow(
+    column(12, 
+           actionButton("mostrar_Ida", "Trayectos de ida"),
+           actionButton("mostrar_Regreso", "Trayectos de regreso")
+    )
+  ),
+  
+  # Distribución principal: Tabla (4 columnas) y Mapa (8 columnas)
+  fluidRow(
+    column(4, 
            tableOutput("tabla_resultados")
     ),
-    column(7,  # Ocupa el espacio restante (8 de 12 columnas)
-           leafletOutput("mapa", height = "600px")
+    column(8, 
+           leafletOutput("mapa"),
+           column(6, plotOutput("grafico1")),
+           column(6, plotOutput("grafico2"))
     )
-  )
+  ),
+  
 )
